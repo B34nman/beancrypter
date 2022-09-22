9f04444575dc1b98662aee79cec050177eacf288
Andy <61066242+B34nman@users.noreply.github.com>
2022-09-22 12:36:57 -0700

    Add files via upload

---
+++ BeanCrypter-File/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace BeanCrypter_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.file_select = new System.Windows.Forms.Button();
+            this.file_path = new System.Windows.Forms.TextBox();
+            this.decrypt = new System.Windows.Forms.Button();
+            this.encrypt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // file_select
+            // 
+            resources.ApplyResources(this.file_select, "file_select");
+            this.file_select.BackColor = System.Drawing.Color.Gray;
+            this.file_select.Name = "file_select";
+            this.file_select.UseVisualStyleBackColor = false;
+            this.file_select.Click += new System.EventHandler(this.file_select_Click);
+            // 
+            // file_path
+            // 
+            resources.ApplyResources(this.file_path, "file_path");
+            this.file_path.BackColor = System.Drawing.Color.White;
+            this.file_path.Name = "file_path";
+            // 
+            // decrypt
+            // 
+            resources.ApplyResources(this.decrypt, "decrypt");
+            this.decrypt.BackColor = System.Drawing.Color.Gray;
+            this.decrypt.Name = "decrypt";
+            this.decrypt.UseVisualStyleBackColor = false;
+            this.decrypt.Click += new System.EventHandler(this.decrypt_Click);
+            // 
+            // encrypt
+            // 
+            resources.ApplyResources(this.encrypt, "encrypt");
+            this.encrypt.BackColor = System.Drawing.Color.Gray;
+            this.encrypt.Name = "encrypt";
+            this.encrypt.UseVisualStyleBackColor = false;
+            this.encrypt.Click += new System.EventHandler(this.encrypt_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Name = "label2";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.Black;
+            this.CausesValidation = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.file_path);
+            this.Controls.Add(this.encrypt);
+            this.Controls.Add(this.decrypt);
+            this.Controls.Add(this.file_select);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button file_select;
+        private System.Windows.Forms.TextBox file_path;
+        private System.Windows.Forms.Button decrypt;
+        private System.Windows.Forms.Button encrypt;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BeanCrypter-File/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BeanCrypter_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool encrypted = false, decrypted = false, fileSelected = false;
+        string contents;
+        private const string SecurityKey = "andersoncooper360_231dsf29fajlasnvs[21301d";
+
+        private void file_select_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Text (*.txt)|*.txt";
+                ofd.Title = "Select text file to encrypt";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    file_path.Text = ofd.FileName;
+                    contents = File.ReadAllText(@file_path.Text);
+                    fileSelected = true;
+                }
+            }
+        } 
+        
+
+        //encrypt
+        private void encrypt_Click(object sender, EventArgs e)
+        {
+            if (!encrypted && fileSelected)
+            {
+                string encrypted_contents = EncryptPlainTextToCipherText(contents);
+                File.WriteAllText(@file_path.Text, encrypted_contents);
+                System.Windows.Forms.MessageBox.Show("File has been encrypted");
+            }
+            else if (!fileSelected)
+                System.Windows.Forms.MessageBox.Show("Please select a file");
+            else
+                System.Windows.Forms.MessageBox.Show("File is already encrypted");
+            encrypted = true;
+            decrypted = false;
+        }
+       
+        //decrypt
+        private void decrypt_Click(object sender, EventArgs e)
+        {
+            if (!decrypted && fileSelected)
+            {
+                contents = File.ReadAllText(@file_path.Text);
+                string decrypted_contents = DecryptCipherTextToPlainText(contents);
+                File.WriteAllText(@file_path.Text, decrypted_contents);
+                System.Windows.Forms.MessageBox.Show("File has been decrypted");
+            }
+            else if (!fileSelected)
+                System.Windows.Forms.MessageBox.Show("Please select a file");
+            else
+                System.Windows.Forms.MessageBox.Show("File is already decrypted");
+            decrypted = true;
+            encrypted = false;
+        }
+
+
+
+        //This method is used to convert the plain text to Encrypted/Un-Readable Text format.
+        public static string EncryptPlainTextToCipherText(string PlainText)
+        {
+            // Getting the bytes of Input String.
+            byte[] toEncryptedArray = UTF8Encoding.UTF8.GetBytes(PlainText);
+
+            MD5CryptoServiceProvider objMD5CryptoService = new MD5CryptoServiceProvider();
+            //Gettting the bytes from the Security Key and Passing it to compute the Corresponding Hash Value.
+            byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(SecurityKey));
+            //De-allocatinng the memory after doing the Job.
+            objMD5CryptoService.Clear();
+
+            var objTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+            //Assigning the Security key to the TripleDES Service Provider.
+            objTripleDESCryptoService.Key = securityKeyArray;
+            //Mode of the Crypto service is Electronic Code Book.
+            objTripleDESCryptoService.Mode = CipherMode.ECB;
+            //Padding Mode is PKCS7 if there is any extra byte is added.
+            objTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+
+            var objCrytpoTransform = objTripleDESCryptoService.CreateEncryptor();
+            //Transform the bytes array to resultArray
+            byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptedArray, 0, toEncryptedArray.Length);
+            objTripleDESCryptoService.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        //This method is used to convert the Encrypted/Un-Readable Text back to readable  format.
+        public static string DecryptCipherTextToPlainText(string CipherText)
+        {
+            byte[] toEncryptArray = Convert.FromBase64String(CipherText);
+            MD5CryptoServiceProvider objMD5CryptoService = new MD5CryptoServiceProvider();
+
+            //Gettting the bytes from the Security Key and Passing it to compute the Corresponding Hash Value.
+            byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(SecurityKey));
+            objMD5CryptoService.Clear();
+
+            var objTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+            //Assigning the Security key to the TripleDES Service Provider.
+            objTripleDESCryptoService.Key = securityKeyArray;
+            //Mode of the Crypto service is Electronic Code Book.
+            objTripleDESCryptoService.Mode = CipherMode.ECB;
+            //Padding Mode is PKCS7 if there is any extra byte is added.
+            objTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+            var objCrytpoTransform = objTripleDESCryptoService.CreateDecryptor();
+            //Transform the bytes array to resultArray
+            byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            objTripleDESCryptoService.Clear();
+
+            //Convert and return the decrypted data/byte into string format.
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+
+    }
+}
